79aafefd934563227f24b9c17f1fa9a7c839c23f
winshabiha <sabiha3289@gmail.com>
2024-10-17 19:56:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// console.log('Hi its me Sabiha Jahan')
+// function sabiha() {
+//     // console.log('Hi its me Sabiha Jahan') 
+//     document.body.style.backgroundColor = 'black'
+// }
+
+// const sabiha = document.getElementById('btn').addEventListener('click',document.body.style.backgroundColor = 'aqua')
+// console.log(sabiha)
+
+// document.getElementById('btn').addEventListener('click',function sabiha(){
+//     document.body.style.backgroundColor = 'black'
+// })
+
+// const title = document.getElementById('btn')
+// title.addEventListener('click',function (){
+//     document.body.style.backgroundColor = 'purple'
+// })
+
+const title = document.getElementById('btn')
+title.addEventListener('click',function(){
+    const para = document.getElementById('para')
+    para.innerText = 'Hi its me Sabiha Jahan'
+})
+
+// const sabiha = document.getElementById('btn')
+// sabiha.addEventListener('click', function(){
+//     const value = document.getElementById('para')
+//     console.log(value)
+// })
